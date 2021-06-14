a1637b56c14f96680c1817ff57cdffb6db37a3f4
Willames Alves <wyllamesro@gmail.com>
2021-06-14 20:13:48 -0300

    add toggle button dark/light

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { StyleSheet } from 'react-native'
+
+import { Switch } from 'react-native-paper'
 
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
@@ -13,6 +16,7 @@ interface Task {
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [theme, setTheme] = useState(false)
 
   function handleAddTask(newTaskTitle: string) {
     if (newTaskTitle === '') {
@@ -51,6 +55,13 @@ export function Home() {
     <>
       <Header />
       <TodoInput addTask={handleAddTask} />
+
+      <Switch
+        value={theme}
+        onValueChange={() => setTheme(!theme)}
+        style={styles.toggleButton}
+      />
+
       <MyTasksList
         tasks={tasks}
         onPress={handleMarkTaskAsDone}
@@ -59,3 +70,11 @@ export function Home() {
     </>
   )
 }
+const styles = StyleSheet.create({
+
+  toggleButton: {
+    position: 'absolute',
+    top: 48,
+    left: 40,
+  }
+})
